b86035085fb2b3335650446c80294ecfa1d78494
Insu Park <facessu@gmail.com>
2023-05-31 13:33:02 +0900

    How to generate Previews Dynamically (Section 8 END)

--- BuildingListsAndNavigation/StartingPoint/Landmarks/Views/LandmarkList.swift
+++ BuildingListsAndNavigation/StartingPoint/Landmarks/Views/LandmarkList.swift
@@ -44,6 +44,7 @@ import SwiftUI
  - List 는 식별 가능한 데이터와 함꼐 작동해요. 두 가지 방식 중 하나를 선택해 식별 가능한 데이터를 만들 수 있어요. 하나는 유니크하게 각 요소를 식별할 수 있는 하나의 프로퍼티로 데이터와 함께 하나의 key path 를 전달하는 것과, 여러분의 데이터 타입을 identifiable 프로토콜을 준수하도록 만드는 방법이 있어요.
  **/
 
+/*
 struct LandmarkList: View {
     var body: some View {
 //        List(landmarks, id: \.id) { landmark in
@@ -75,3 +76,59 @@ struct LandmarkList_Previews: PreviewProvider {
         LandmarkList()
     }
 }
+ */
+
+/*
+ Section 8 - Generate Previews Dynamically
+ 
+ 다음으로 LandmarkList_Previews preview provider 에 코드를 추가할건데 다른 디바이스 사이즈에 리스트 뷰의 프리뷰를 렌더하기 위함이죠. 기본적으로 프리뷰는 active scheme 내에서 디바이스의 사이즈를 렌더링해요.
+ 프리뷰의 디바이스를 previewDevice(_:) modifier method 를 호출하여 변경할 수 있어요.
+ */
+
+struct LandmarkList: View {
+    var body: some View {
+//        List(landmarks, id: \.id) { landmark in
+//            LandmarkRow(landmark: landmark)
+//        }
+        
+        // Landmark 에 identifiable 프로토콜을 추가했기 때문에 id 파라미터를 삭제할 수 있다.
+        // Section 6 - Navigation View 추가
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            
+            .navigationTitle("Landmarks")
+            // Section 6 End
+        }
+        
+        // 이제 Landmarks collection 에 직접 접근하여 사용 할 수 있다.
+        // Section 5 End
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        /*
+            Xcode’s scheme menu 에 있는 모든 디바이스의 이름을 사용할 수 있어요.
+            즉, iOS Simulator 에 기기가 추가되어 있어야 합니다.
+         
+            < ForEach >
+            ForEach 는 Collection 타입에서 리스트와 같은 방식으로 작동하는데요, 이것은 child view(in stacks, lists, groups 등) 를 사용할 수 있는 어느 곳이든 ForEach 를 쓸 수 있다는 것을 의미해요. 여러분의 데이터의 요소들이 단순 values types 일 때(여기서 사용한 string 과 같이), 여러분은 \.self 를 key path 로서 identifier 로 사용할 수 있어요.
+         
+            < .previewDisplayName(deviceName) >
+            디바이스 이름을 프리뷰에 레이블로 추가하세요. 여러 프리뷰를 추가할 때 디바이스 명으로 출력이 되기 때문에 매우 보기가 편해져요.
+         */
+        
+        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+    }
+}
+
