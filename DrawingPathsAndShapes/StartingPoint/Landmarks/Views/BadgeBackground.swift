f89d99221d95722a15852f116ed83eac2de63a08
Insu Park <facessu@gmail.com>
2023-06-03 01:21:23 +0900

    Section 2 - Draw the Badge Background (Too hard, Not Recommended to understand)

---
+++ DrawingPathsAndShapes/StartingPoint/Landmarks/Views/BadgeBackground.swift
@@ -0,0 +1,63 @@
+//
+//  BadgeBackground.swift
+//  Landmarks
+//
+//  Created by Insu Park on 2023/06/02.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        GeometryReader { geometry in
+            Path { path in
+                var width: CGFloat = min(geometry.size.width, geometry.size.height)
+                let height = width
+                let xScale: CGFloat = 0.832
+                let xOffSet = (width * (1.0 - xScale)) / 2.0
+                width *= xScale
+                path.move(
+                    to: CGPoint(
+                        x: width * 0.95 + xOffSet,
+                        y: height * (0.20 + HexagonParameters.adjustment)
+                    )
+                )
+                
+                HexagonParameters.segments.forEach { segment in
+                    path.addLine(
+                        to: CGPoint.init(
+                            x: width * segment.line.x + xOffSet,
+                            y: height * segment.line.y
+                        )
+                    )
+                    
+                    path.addQuadCurve(
+                        to: CGPoint(
+                            x: width * segment.curve.x + xOffSet,
+                            y: height * segment.curve.y
+                        ),
+                        control: CGPoint(
+                            x: width * segment.control.x + xOffSet,
+                            y: height * segment.control.y
+                        )
+                    )
+                }
+            }
+            .fill(.linearGradient(
+                Gradient(colors: [Self.gradientStart, Self.gradientEnd]),
+                startPoint: UnitPoint.init(x: 0.5, y: 0),
+                endPoint: UnitPoint(x: 0.5, y: 0.6)))
+        }
+        .aspectRatio(1, contentMode: .fit)
+    }
+    
+    static let gradientStart = Color(red: 239.0 / 255, green: 120.0 / 255, blue: 221.0 / 255)
+    static let gradientEnd = Color(red: 239.0 / 255, green: 172.0 / 255, blue: 120.0 / 255)
+}
+
+struct BadgeBackground_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeBackground()
+    }
+}
--- DrawingPathsAndShapes/StartingPoint/README.md
+++ DrawingPathsAndShapes/StartingPoint/README.md
@@ -53,3 +53,65 @@ CoreGraphics 를 Import 하면 CGPoint 를 사용할 수 있어요.
 ### Step 7 
 
 Adjustment 값을 추가할게요. 육각형의 모양을 조율하는 역할이에요. 
+
+
+## Section 2 - 뱃지 배경 그리기 (Draw the Badge Background)
+
+SwiftUI 의 Graphics API 를 사용해 커스텀 뱃지 모양을 그려볼게요.
+
+### Step 1
+
+파일을 생성할게요. SwiftUI View 템플릿을 선택해 BadgeBackground 이름으로 생성해주세요.
+
+### Step 2
+
+생성한 파일로 가서, 'Path' 뱃지의 모양을 추가하고 fill() modifier 를 뷰 내에서 모양을 나타내기 위해 적용해주세요.
+
+Paths 의 용도는 선과 곡선과 뱃지의 육각형 배경과 같이 더 복잡한 모양의 형태를 만들기 위한 다른 원시적인 드로잉 요소들을 결합하는데 사용돼요. (You use paths to combine lines, curves, and other drawing primitives to form more complex shapes like the badge’s hexagonal background.)
+
+### Step 3
+
+Path에 시작 점을 추가해요. 컨테이너의 사이즈는 100X100 으로 가정해보죠.
+
+move(to:) 메서드는 가상의 펜이나 연필이 해당 영역 위를 맴돌며 그리기 시작하기를 기다리는 것처럼 도형의 경계 내에서 드로잉 커서를 이동합니다.
+
+### Step 4
+
+러프한 육각형 모양을 생성하기 위해 모양 데이터(shape data)의 각 점에 대해 선을 그립니다.
+
+addList(to:) 메서드는 단일 점을 가져와서 그린다. addLine(to:)에 대한 연속적인 호출은 이전 점에서 선을 시작하고 새 점으로 계속됩니다.
+
+
+* 육각형이 좀 이상해도 걱정마세요. 모양의 모퉁이에서 각각의 선분(segment)의 곡선 부분을 무시했기 때문이에요.  
+
+### Step 5
+
+addQuadCurve(to:control:) 메서드를 사용해서 베지어 곡선을 뱃지의 모퉁이에 그려볼게요.
+
+
+### Step 6
+
+Badge가 값(100)을 하드 코딩하는 대신 크기를 정의하는 포함된 뷰의 크기를 사용할 수 있도록 GeometryReader 를 이용해 Path 코드를 래핑합니다.
+
+기하학 2차원의 가장 작은 것을 사용하면 포함하는 뷰가 정사각형이 아닐 때 배지의 종횡비가 유지됩니다. (?????)
+(Using the smallest of the geometry’s two dimensions preserves the aspect ratio of the badge when its containing view isn’t square.)
+
+
+### Step 7
+
+xScale 을 사용해 x축의 모양의 크기를 조정한 다음 xOffSet 을 추가하여 형상 내에서 모양을 중앙에 맞춥니다.
+(Scale the shape on the x-axis using xScale, and then add xOffset to recenter the shape within its geometry.)
+
+### Step 8
+
+Fill 코드 블록을 다음과 같이 변경해 그라디언트 효과롤 부여합니다.
+
+
+### Step 9
+
+aspectRatio(_:contentMode:) modifier 를 적용해 Gradient를 가득 채워보세요.
+
+1:1 종횡비를 유지함으로서 배지는 뷰의 중심 위치를 유지합니다. 부모 뷰가 정사각형이 아니더라도요.
+
+
+
